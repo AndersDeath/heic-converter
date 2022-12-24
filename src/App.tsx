0dd3c5d9a1e68ead8b0969470744ba4a45d64f32
Vladimir Bolshakov <andersdeath@yandex.ru>
2022-12-24 13:44:25 +0400

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const fileInput = useRef(null);
+
+  const handleClick = () => {
+    (fileInput as any).current.click();
+  };
+
+  const handleChange = (e: any) => {
+    console.log(e.target.files[0]);
+  };
+
   return (
     <div className="App">
-      <div className="button input"></div>
-      <div className="button output"></div>
+      <div className="button input" onClick={handleClick}> Upload</div>
+      <input
+        type="file"
+        ref={fileInput}
+        style={{ display: 'none' }}
+        onChange={handleChange}
+      />
+      <div className="button output">Download</div>
     </div>
   );
 }
